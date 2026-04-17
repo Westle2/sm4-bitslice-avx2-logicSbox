182b4b86abb4e8deed3f95d460776cc6dc9ce9fe
RH S <166029372+Westle2@users.noreply.github.com>
2026-04-17 13:46:54 +0800

    Create benchmark.c

---
+++ src/benchmark.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+void benchmark(){
+  uint8_t key[16]={0};
+  uint8_t in[16*1024]={0};
+  uint8_t out[16*1024]={0};
+  uint32_t rk[32];
+  sm4_setkey(key,rk);
+  clock_t t1=clock();
+  for(int i=0;i<200;i++)
+    sm4_encrypt_ecb(in,out,1024,rk);
+  clock_t t2=clock();
+  double s=(double)(t2-t1)/CLOCKS_PER_SEC;
+  printf("SM4 ECB 16KB x200: %.3fs\n",s)
+    ;printf("Approx Throughput: %.2f MB/s\n",(16.0*1024*200/1024/1024)/s);
+} 
