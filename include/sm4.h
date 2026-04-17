8c55fa3c9c2c05d07c99d7520f383f82b6593dd9
RH S <166029372+Westle2@users.noreply.github.com>
2026-04-17 14:10:18 +0800

    Update sm4.h

--- include/sm4.h
+++ include/sm4.h
@@ -1,9 +1,31 @@
 #ifndef SM4_H
 #define SM4_H
+
 #include <stdint.h>
-void sm4_setkey(const uint8_t key[16], uint32_t rk[32]);
-void sm4_encrypt_block(const uint8_t in[16], uint8_t out[16], const uint32_t rk[32]);
-void sm4_encrypt_ecb(const uint8_t *in, uint8_t *out, int blocks, const uint32_t rk[32]);
-void sm4_encrypt_avx2_demo(const uint32_t *in, uint32_t *out);
-void benchmark();
+
+#define SM4_BLOCK_SIZE 16
+#define SM4_ROUNDS     32
+
+typedef struct
+{
+    uint32_t rk[32];
+} sm4_key_t;
+
+/* key schedule */
+void sm4_setkey_basic(
+    const uint8_t key[16],
+    uint32_t rk[32]);
+
+/* basic */
+void sm4_encrypt_block_basic(
+    const uint8_t in[16],
+    uint8_t out[16],
+    const uint32_t rk[32]);
+
+void sm4_encrypt_ecb_basic(
+    const uint8_t *in,
+    uint8_t *out,
+    int blocks,
+    const uint32_t rk[32]);
+
 #endif
